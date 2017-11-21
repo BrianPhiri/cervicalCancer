ca81072080c894a8b3d706c0d04cf4e53c84011e
ejakait <ejakaitcaleb@gmail.com>
2017-11-21 11:40:13 +0300

    app file changes

--- server/app.py
+++ server/app.py
@@ -3,18 +3,19 @@ from forms import ContactForm
 app = Flask(__name__)
 app.secret_key = 'development key'
 
-@app.route('/contact', methods = ['GET', 'POST'])
+
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-   form = ContactForm()
-   
-   if request.method == 'POST':
-      if form.validate() == False:
-         flash('All fields are required.')
-         return render_template('contact.html', form = form)
-      else:
-         return render_template('success.html')
-      elif request.method == 'GET':
-         return render_template('contact.html', form = form)
+    form = ContactForm()
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            flash('All fields are required.')
+            return render_template('contact.html', form=form)
+        else:
+            return render_template('success.html')
+    return render_template('contact.html', form=form)
+    
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
